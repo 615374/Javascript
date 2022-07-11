b4da3a9c8eb52b8bcd70116efd6b3b7b74dfb3e9
615374 <giselalanzillotta@gmail.com>
2022-07-11 20:23:29 -0300

    arrays

--- miarchivo.js
+++ miarchivo.js
@@ -1,130 +1,100 @@
-function saludar(nombre = "usuario", apellido = " "){
-    alert(`Hola ${nombre} ${apellido}, bienvenido a "Mi tienda Online" `);
-}
-
-saludar();
+const arrayProductos=[];
+const arrayCarrito =[];
+const prenda= prenda
 
 
-function solicitarAnswer(){
-const answer = prompt(`Deseas realizar una compra?:
-1: Si
-2: No
-`);
-
-if (answer == 1) {
-    alert ("Has respondido que Si");
-
-} else if(answer == 2) {
-    alert ("Has respodido que No. Gracias por visitar Mi tienda online");
-
-} else {
-    alert ("error al ingresar respuesta")    
-}
-console.log(answer)
-}
-
-solicitarAnswer();
-
-
-const option = prompt(`Elija un producto: 
-remera 
-pantalon 
-campera 
-`);
-
-let precio1 = 2500;
-let precio2 = 3800;
-let precio3 = 4500;
-
-alert ("Has elegido: " + option)
-
-switch (option){
-    case "remera":
-        alert(`producto ${option} = $ ${precio1} `);
-        break;
-    case "pantalon":
-        alert(`producto ${option} = $ ${precio2}`);
-        break;
-    case "campera":
-        alert(`producto ${option} = $ ${precio3}`);
-        break;
-    default:
-        alert("No ingresaste ningun producto valido");
-        break;
-} 
-
-let precio = parseInt (prompt("Ingresa el precio del producto seleccionado:"));
-let formadePago, cuotas;
-let total = 0;
-let cant = 0;
-
-let formaPago = parseInt(prompt("Como desea pagar? 1- Contado 2- Credito"));
-
-
-if (formaPago == 1) {
-    alert ("Total a pagar: $"+precio);
-
-} else if(formaPago == 2) {
-    cuotas = parseInt(prompt("Ingrese cantidad de cuotas 6 o 12"));
-    total = credito(precio, cuotas);
-    //let cuota = (precio/cuotas)
-    alert("Total: $"+precio+ "//" + cuotas + "cuotas de $"+(precio/cuotas));
-
-} else {
-    alert ("error al ingresar respuesta")    
-}   
-
-function credito(precio,cuotas){
-    switch (cuotas) {
-        case (6):
-            total = precio * 1.2;
-            break;
-
-        case (12):
-            total = precio * 1.35;
-            break;
-    
-        default:
-            alert:("error al ingresar cuotas");
-            break;
+class Producto{
+    constructor(nombre, precio, stock) {
+        this.precio = precio;
+        this.stock = stock;
+        this.nombre = nombre;
     }
-
- return total;
-
+}
+class Carrito{
+  constructor(nombre, precio) {
+      this.precio = precio;
+      this.nombre = nombre;
+  }
 }
 
+let j=0;
+arrayProductos[j++]= new Producto("Remera", 2500, 5);
+arrayProductos[j++]= new Producto("Pantalon", 3800, 4);
+arrayProductos[j++]= new Producto("Campera", 4500, 2);
 
-const nombre = "Remera";
-const valor = 2500;
-const disponible = true;
 
+function seleccionCarrito(prenda) {
+  switch (prenda) {
+    case 0:
+      if (arrayProductos[prenda].stock < 1) {
+        alert("Remera no disponible, sin stock momentaneamente");  
+      } else {
+        
+        agregarCarrito(arrayProductos[prenda],arrayCarrito);
 
-//Object constructor
-
-const producto = {
-    nombre: "Remera",
-    valor: 2500,
-    disponible: true,
-};
-
-function Producto(nombre, valor){
-    this.nombre = nombre;
-    this.valor = valor;
-    this.disponible = true;
+        alert("Remera agregada al carrito");
+        console.log("Remera agregada al carrito");
+      }
+      console.log(arrayCarrito);
+  
+    
+      break;
+    case 1:
+      if (arrayProductos[prenda].stock < 1) {
+        alert("Pantalon no disponible, sin stock momentaneamente");  
+      } else {
+        
+        agregarCarrito(arrayProductos[prenda],arrayCarrito);
+        
+        alert("Pantalon agregado al carrito");
+        console.log("Pantalon agregado al carrito");
+      }
+      console.log(arrayCarrito);
+      
+      break;
+    case 2:
+      
+      if (arrayProductos[prenda].stock < 1) {
+        alert("Campera no disponible, sin stock momentaneamente");  
+      } else {
+        
+        agregarCarrito(arrayProductos[prenda],arrayCarrito);
+        
+        alert("Campera agregada al carrito");
+        console.log("Campera agregada al carrito");
+      }
+      console.log(arrayCarrito);
+      
+      break;
+    default:
+        alert("error de producto")
+      break;
+  }
 }
 
-const producto2= new Producto("Pantalon, 3800");
-const producto3= new Producto ("Campera, 4500");
+function agregarCarrito(objeto,arrayCarrito) {
 
-console.log(producto,producto2,producto3)
+  let n =objeto.nombre
+  let p =objeto.precio;
+  const a = new Carrito(n,p);
+  arrayCarrito.push(a);
+}
 
-const carrito = [
-    { producto: 'Remera', precio: 2500},
-    { producto: 'Pantalon', precio: 3800},
-    { producto: 'Campera', precio: 4500},
-   
-];
+function calculaCompra(){
+  let monto=0;
+    for (let index = 0; index < arrayCarrito.length; index++) {
+      monto+= arrayCarrito[index].precio;
+    }
+    return monto;
+}
 
-console.log(carrito);
+function finalizar() {
+
+    console.log("Los productos del carrito son:");
+    for (let index = 0; index < arrayCarrito.length; index++) {
+      console.log("-"+arrayCarrito[index].nombre)
+    } 
+    alert(`El total de su compra es: $${(calculaCompra().toFixed(2))}`);  
+}
 
 
